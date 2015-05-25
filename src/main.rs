29c217f8056ef4ef71d6f528ce7036b68c672fde
Corey Farwell <coreyf@rwell.org>
2015-05-25 16:45:53 -0400

    Init hello world via tiny_http

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use std::env;
+
+extern crate tiny_http;
+
+
 fn main() {
-    println!("Hello, world!");
+    let port = match env::var("PORT") {
+        Ok(p) => p.parse::<u16>().unwrap(),
+        Err(..) => 8000,
+    };
+
+    let server = tiny_http::ServerBuilder::new().with_port(port).build().unwrap();
+
+    for request in server.incoming_requests() {
+        println!("received request! method: {:?}, url: {:?}, headers: {:?}",
+            request.get_method(),
+            request.get_url(),
+            request.get_headers()
+        );
+
+        let response = tiny_http::Response::from_string("hello world".to_string());
+        request.respond(response);
+    }
 }
