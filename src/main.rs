386eb8fb9fb2662b35d786ba1b6e693323209179
Corey Farwell <coreyf@rwell.org>
2015-05-28 23:07:07 -0400

    Explicitly specify root node

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,6 @@
 /* TODO:
  *  * redirect root url
  *  * add watermark? url to repo?
- *  * specify root node
  *  * update git repo?
  *  * serverside caching? redis?
  */
@@ -39,6 +38,8 @@ fn build_dot(crate_name: &str, dep_map: &HashMap<String, Vec<String>>) -> Vec<u8
     let mut dot = String::new();
     dot.push_str("digraph graphname {");
     dot.push_str("ratio=0.75;");
+
+    dot.push_str(&format!("\"{}\" [root=true]", crate_name));
 
     // Which dependencies we've already seen
     let mut seen_set = HashSet::new();
