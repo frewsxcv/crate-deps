805102bca644d5446fb58ebeabf323458b5aa669
Corey Farwell <coreyf@rwell.org>
2015-05-28 20:53:11 -0400

    Bump tiny_http version: 0.1.1 -> 0.2.0

--- src/main.rs
+++ src/main.rs
@@ -85,7 +85,7 @@ fn main() {
     println!("Server listening on port {}", port);
     for req in server.incoming_requests() {
         let response = {
-            let crate_name = req.get_url().trim_left_matches("/");
+            let crate_name = req.url().trim_left_matches("/");
             if dep_map.get(crate_name).is_some() {
                 let data = build_dot(crate_name, &dep_map);
                 let content_type_header = "Content-Type: image/png".parse::<Header>().unwrap();
